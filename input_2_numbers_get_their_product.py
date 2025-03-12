9293e6227a5692fef29109d42937b5b91718bc4f
Rai <railynmagondaya@gmail.com>
2025-03-12 22:14:33 +0800

    Finished the program about asking the user to input two numbers and printing their product, pseudocode given before the program

---
+++ input_2_numbers_get_their_product.py
@@ -0,0 +1,11 @@
+#ask user to input float numbers
+#check if given value is valid
+#product = num1 * num2
+#print "the product is <product>"
+
+print("We'll multiply the two numbers you're going to input")
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+product = (num1 * num2)
+print("The product of the two numbers is", product)
