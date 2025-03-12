2364fe56eea7a24f81a84959cfb2f4c57a2a29bc
Rai <railynmagondaya@gmail.com>
2025-03-12 21:42:07 +0800

    Fixed it that the only thing that's gonna print if the condition is met is the number only.

--- input_2_numbers_print_the_bigger_number.py
+++ input_2_numbers_print_the_bigger_number.py
@@ -5,12 +5,13 @@
 #ifelse num 2 > num 1: "<num2> is bigger than <num1>"
 #else: print "they're the same, no amount is bigger"
 
+print("input two numbers and we'll print what's bigger")
 num1 = float(input("Number 1: "))
 num2 = float(input("Number 2: "))
 
 if num1 > num2:
-    print(num1, "is bigger than", num2)
+    print(num1)
 if num2 > num1:
-    print(num2, "is bigger than", num1)
+    print(num2)
 if num2 == num1 :
      print("The two numbers are the same, no bigger number")
