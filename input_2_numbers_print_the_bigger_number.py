d3f6b15b396c16aabf8f2a754081e367ab883d90
Rai <railynmagondaya@gmail.com>
2025-03-12 21:32:10 +0800

    Finished the if statements, printing what number is bigger or if there's no bigger number

--- input_2_numbers_print_the_bigger_number.py
+++ input_2_numbers_print_the_bigger_number.py
@@ -4,3 +4,13 @@
 #if num1 > num 2: print "<num1> is bigger than <num 2>'
 #ifelse num 2 > num 1: "<num2> is bigger than <num1>"
 #else: print "they're the same, no amount is bigger"
+
+num1 = float(input("Number 1: "))
+num2 = float(input("Number 2: "))
+
+if num1 > num2:
+    print(num1, "is bigger than", num2)
+if num2 > num1:
+    print(num2, "is bigger than", num1)
+if num2 == num1 :
+     print("The two numbers are the same, no bigger number")
