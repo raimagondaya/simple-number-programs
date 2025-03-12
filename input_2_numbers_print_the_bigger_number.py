c6e7959f30ea07db4be6eb29ff173a853b5fe526
Rai <railynmagondaya@gmail.com>
2025-03-12 14:01:14 +0800

    Batch 1 number 1 pseudocode

---
+++ input_2_numbers_print_the_bigger_number.py
@@ -0,0 +1,6 @@
+#pseudocode ig
+#ask user to input float nnumbers
+#check if given value is valid
+#if num1 > num 2: print "<num1> is bigger than <num 2>'
+#ifelse num 2 > num 1: "<num2> is bigger than <num1>"
+#else: print "they're the same, no amount is bigger"
