830704da2c0d0d4ab9aa88a84218a8e6bca19b70
Rai <railynmagondaya@gmail.com>
2025-03-12 22:49:21 +0800

    Finished the program that asks the user to input 10 numbers and it'll add them all and print its sum. pseudocode given before the program.

---
+++ input_10_numbers_get_their_sum.py
@@ -0,0 +1,21 @@
+#ask the user to input 10 numbers
+#make sure input is valid
+#use try-except blocks
+#enter num 1 - 10 using loop
+#total = (num 1 - 10)
+#print total
+
+print("Enter 10 numbers and we will add all of them and print the sum:1")
+
+total = 0
+
+for i in range(10):
+    while True:
+        try:
+            num = float(input(f"Enter number {i+1}: "))
+            total += num
+            break
+        except ValueError:
+            print("Enter a valid number.")
+
+print(f"The sum is: ", total)
