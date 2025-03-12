adc4eff8873e4a46ed515c46482706f0bba14cbb
Rai <railynmagondaya@gmail.com>
2025-03-12 23:06:01 +0800

    Finished the program that asks the user for 10 numbers and it will print how many of those numbers are odd numbers. pseudocode just above the program.

---
+++ input_10_numbers_print_how_many_are_odd.py
@@ -0,0 +1,18 @@
+#user input 10 numbers
+#for loop from num1 - num10
+#check if num n is divisible by 2
+#if yes, add to the odd number count
+#repeat until reaching num 10
+#print "there are <odd number counted> odd numbers"
+
+print("You will input 10 numbers and the program will count how many of those numbers are odd.")
+
+odd_numbers = 0
+
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    
+    if num % 2 != 0:  # Check if the number is odd
+        odd_numbers += 1
+
+print(f"There are {odd_numbers} odd numbers")
