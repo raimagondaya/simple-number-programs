a51142f4abdcd4f193d5792f0f86d2120fd3b926
Rai <railynmagondaya@gmail.com>
2025-03-12 22:06:05 +0800

    wrote the pseudocode and finished the input_2_numbers_and_add_print_sum.py

---
+++ input_2_numbers_and_add_print_sum.py
@@ -0,0 +1,11 @@
+#ask user to input float numbers
+#check if given value is valid
+#sum = num1 + num2
+#print "the sum is <sum>"
+
+print("We'll add the two numbers you're going to input")
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+sum = (num1 + num2)
+print("The sum of the two numbers is", sum)
