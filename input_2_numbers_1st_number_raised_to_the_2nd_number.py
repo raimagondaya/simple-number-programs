4e829fb094979c9bb32f1f3de583e77816e6a929
Rai <railynmagondaya@gmail.com>
2025-03-12 22:32:23 +0800

    Finished the program that asks the user to input two numbers; the first number will be raised to the power of the second number and it will print the result, pseudocode given before the program.

---
+++ input_2_numbers_1st_number_raised_to_the_2nd_number.py
@@ -0,0 +1,11 @@
+#ask user to input float numbers
+#check if given value is valid
+#result = num1 ** num2
+#print "the result is <result>"
+
+print("We'll raise the 1st number you're going to input to the power of the second number you will input. ")
+num1 = float(input("1st number: "))
+num2 = float(input("2nd number: "))
+
+result = (num1 ** num2)
+print("The result is", result)
